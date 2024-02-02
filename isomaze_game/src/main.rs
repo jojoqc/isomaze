b09d320ade3e4e52853b854aa46fee7e71c94a12
joasquintero <joasquinteroc@gmail.com>
2024-02-01 23:31:31 -0400

    [WIP]

--- isomaze_game/src/main.rs
+++ isomaze_game/src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
+use bevy::window::{PresentMode, WindowMode, WindowPlugin};
+use bevy_framepace::FramepacePlugin;
+use isomaze_lib::graphics::geometry;
+
 fn main() {
-    println!("Hello, world!");
+    App::new()
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            primary_window: Some(Window {
+                title: "isomaze".to_string(),
+                present_mode: PresentMode::AutoNoVsync,
+                mode: WindowMode::Windowed,
+                ..default()
+            }),
+            ..Default::default()
+        }))
+        .add_plugins(FramepacePlugin)
+        // .add_systems(StartUp, setup)
+        // .add_systems(Update,(system, update_config))
+        .run();
 }
+
+fn setup() {}
